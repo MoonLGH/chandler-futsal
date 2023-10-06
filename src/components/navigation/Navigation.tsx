26b39e38c80800e8cc5b3a7aafb55e67b4d7c2bb
Isaac Arnold <67135844+IsaacArnold@users.noreply.github.com>
2023-10-06 10:05:40 +1000

    Installing ChakraUI

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import { Link } from "@chakra-ui/next-js";
 import React from "react";
 
 const Navigation = () => {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,16 @@ import Navigation from "@/components/navigation/Navigation";
 export default function Home() {
   return (
     <main>
-      <h1>Chandler Eagles Futsal</h1>
+      <h1>Welcome to Chandler Eagles Futsal</h1>
       <Navigation />
+      <p>
+        This is the home of Chandler Eagles Futsal, proudly brought to you by
+        Sala Time Futsal.
+      </p>
+      <p>
+        This website is still a work-in-progress, so for now, please check out
+        where your team sits in the league.
+      </p>
     </main>
   );
 }
